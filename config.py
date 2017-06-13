9d2094f703f1852b845f00e753482bebf3a0f016
kirbycm <kirbycm@gmail.com>
2017-06-12 20:56:36 -0400

    Heroku fix testing again.

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database', 'app.db')
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/postgres'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/postgres'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'database', 'migrations')
 OTP_SECRET = 'abc'

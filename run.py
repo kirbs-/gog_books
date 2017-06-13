37381b00481b356062377159931814b67c0396a2
kirbycm <kirbycm@gmail.com>
2017-06-12 20:15:40 -0400

    Heroku fix testing.

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 #!/Users/ckirby/.virtualenvs/gog_books/bin/python
 from app import app
-app.jinja_env.auto_reload = True
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.run(debug=True)
+
+if __name__ == 'main':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True)
